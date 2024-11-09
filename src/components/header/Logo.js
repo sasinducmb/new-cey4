2d4cbdfb4a3fc9d4cb07655ae3b650d0797c8d14
Sasindu Kavishka <141473967+sasinducmb@users.noreply.github.com>
2024-11-09 09:53:54 +0530

    logo 

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -5,8 +5,12 @@ import { Link } from "react-router-dom";
 const Logo = ({ imageUrl, logoClass }) => {
   return (
     <div className={clsx(logoClass)}>
-      <Link to={process.env.PUBLIC_URL + "/"}>
-        <img alt="" src={process.env.PUBLIC_URL + imageUrl} />
+      <Link to={process.env.PUBLIC_URL + '/'}>
+        <img
+          style={{ width: '150px' }}
+          alt=""
+          src={process.env.PUBLIC_URL + imageUrl}
+        />
       </Link>
     </div>
   );
